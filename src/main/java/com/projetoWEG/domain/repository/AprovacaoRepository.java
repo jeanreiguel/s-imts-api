20edaaf1119198d713f377d0882ffeb924cb6846
jenasw <jeanrgl03@gmail.com>
2021-08-24 16:35:45 -0300

    Inicialização da correção de inserção de aprovamento

--- src/main/java/com/projetoWEG/domain/repository/AprovacaoRepository.java
+++ src/main/java/com/projetoWEG/domain/repository/AprovacaoRepository.java
@@ -1,9 +1,9 @@
-//package com.projetoWEG.domain.repository;
-//
-//import com.projetoWEG.domain.model.Aprovacao;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface AprovacaoRepository extends JpaRepository<Aprovacao, Long> {
-//}
+package com.projetoWEG.domain.repository;
+
+import com.projetoWEG.domain.model.Aprovacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AprovacaoRepository extends JpaRepository<Aprovacao, Long> {
+}
