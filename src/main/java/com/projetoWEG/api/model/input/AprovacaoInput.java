3a058c22e48c41248085177527b1a39840edcc4c
jenasw <jeanrgl03@gmail.com>
2021-08-24 16:36:48 -0300

    dados da aprovação

--- src/main/java/com/projetoWEG/api/model/input/AprovacaoInput.java
+++ src/main/java/com/projetoWEG/api/model/input/AprovacaoInput.java
@@ -1,4 +1,27 @@
 package com.projetoWEG.api.model.input;
 
+import com.projetoWEG.domain.model.Apontamento;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
+
 public class AprovacaoInput {
+
+    @Valid
+    @NotNull
+    Date data;
+
+    @Valid
+    @NotBlank
+    String nomeFornecedor;
+
+    List<Apontamento> apontamento;
+
+    @Valid
+    @NotNull
+    double valorHora;
+
 }
