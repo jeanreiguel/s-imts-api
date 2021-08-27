4562f419ddebe70e89ccd579cf3995e59546a279
jenasw <jeanrgl03@gmail.com>
2021-08-27 16:10:13 -0300

    Adicionar find by consultor - não realizado

--- src/main/java/com/projetoWEG/domain/repository/AlocacaoRepository.java
+++ src/main/java/com/projetoWEG/domain/repository/AlocacaoRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AlocacaoRepository extends JpaRepository<Alocacao, Long> {
-    List<Consultor> findByidConsultor(Long idConsultor);
 }
