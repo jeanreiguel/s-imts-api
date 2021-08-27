06abd5f62e084ba775a7689e82a3af5440cd75db
jenasw <jeanrgl03@gmail.com>
2021-08-27 16:11:52 -0300

    Controller de acesso a service de aprovação

--- src/main/java/com/projetoWEG/api/controller/AprovacaoController.java
+++ src/main/java/com/projetoWEG/api/controller/AprovacaoController.java
@@ -1,2 +1,33 @@
-package com.projetoWEG.api.controller;public class AprovacaoController {
+package com.projetoWEG.api.controller;
+
+import com.projetoWEG.api.model.dto.AprovacaoDTO;
+import com.projetoWEG.api.model.input.AprovacaoInput;
+import com.projetoWEG.domain.exception.CasoException;
+import com.projetoWEG.domain.model.Apontamento;
+import com.projetoWEG.domain.model.Aprovacao;
+import com.projetoWEG.domain.repository.ApontamentosRepository;
+import com.projetoWEG.domain.service.ApontamentosService;
+import com.projetoWEG.domain.service.AprovacaoApontamentoService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/aprovacao")
+public class AprovacaoController {
+
+    private AprovacaoApontamentoService aprovacaoApontamentoService;
+    private ApontamentosRepository apontamentosRepository;
+    @GetMapping
+    public List<AprovacaoDTO> listarAprovacoes() {
+        return aprovacaoApontamentoService.listarAprovacoes();
+    }
+
+    @PostMapping("/inserir")
+    public AprovacaoDTO inserirAprovacao(@RequestBody AprovacaoInput aprovacaoInput) {
+
+        return aprovacaoApontamentoService.aprovarHoras(aprovacaoInput);
+    }
 }
