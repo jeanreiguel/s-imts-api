7d6867a8e834b1d781b41acca615d6871eb8af2e
jenasw <jeanrgl03@gmail.com>
2021-09-17 10:52:55 -0300

    Model de seção

--- src/main/java/com/projetoWEG/domain/model/Secao.java
+++ src/main/java/com/projetoWEG/domain/model/Secao.java
@@ -1,2 +1,29 @@
-package com.projetoWEG.domain.model;public class Secao {
+package com.projetoWEG.domain.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity(name = "secoes")
+public class Secao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "secao_id")
+    Long idSecao;
+
+    @Valid
+    @NotBlank
+    @Size(max = 255)
+    @Column(name = "secao_nome")
+    String nomeSecao;
 }
