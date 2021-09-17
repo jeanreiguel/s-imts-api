810f162ea6ea2a681cb9dafa18abe9ac9e1cdad8
jenasw <jeanrgl03@gmail.com>
2021-09-17 10:53:06 -0300

    Model de skill

--- src/main/java/com/projetoWEG/domain/model/Skill.java
+++ src/main/java/com/projetoWEG/domain/model/Skill.java
@@ -1,2 +1,30 @@
-package com.projetoWEG.domain.model;public class Skill {
+package com.projetoWEG.domain.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity(name = "skills")
+public class Skill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "skill_id")
+    Long id;
+
+    @Valid
+    @NotBlank
+    @Size(max = 255)
+    @Column(name = "skill_nome")
+    String nome;
+
 }
