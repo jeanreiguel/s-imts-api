546bb87488e887726bbcc581875c66c7f93d672b
Jean <jeanrgl03@gmail.com>
2021-09-22 13:41:09 -0300

    Adição de pesquisa das seções para não haver repetição

--- src/main/java/com/projetoWEG/api/controller/SecaoController.java
+++ src/main/java/com/projetoWEG/api/controller/SecaoController.java
@@ -1,2 +1,24 @@
-package com.projetoWEG.api.controller;public class SecaoController {
+package com.projetoWEG.api.controller;
+
+import com.projetoWEG.domain.model.Secao;
+import com.projetoWEG.domain.repository.SecaoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/secao")
+public class SecaoController {
+
+    private SecaoRepository secaoRepository;
+
+    @GetMapping
+    public List<Secao> listarSecao() {
+        return secaoRepository.findAll();
+    }
 }
--- src/main/java/com/projetoWEG/domain/repository/SecaoRepository.java
+++ src/main/java/com/projetoWEG/domain/repository/SecaoRepository.java
@@ -1,2 +1,12 @@
-package com.projetoWEG.domain.repository;public interface SecaoRepository {
+package com.projetoWEG.domain.repository;
+
+import com.projetoWEG.domain.model.Secao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SecaoRepository extends JpaRepository<Secao, Long> {
+    List<Secao> findByNomeSecao(String nome);
 }
