2da22bbabff97214f1db54971943465b147ffd03
Jean <jeanrgl03@gmail.com>
2021-09-22 15:38:05 -0300

    Organização na service de projeto

--- src/main/java/com/projetoWEG/domain/service/ProjetoService.java
+++ src/main/java/com/projetoWEG/domain/service/ProjetoService.java
@@ -1,7 +1,7 @@
 package com.projetoWEG.domain.service;
 
 import com.projetoWEG.api.assembler.ProjetoAssembler;
-import com.projetoWEG.api.model.dto.ProjetoFornecedorDTO;
+import com.projetoWEG.api.model.dto.projeto.ProjetoFornecedorDTO;
 import com.projetoWEG.domain.exception.CasoException;
 import com.projetoWEG.domain.model.Projeto;
 import com.projetoWEG.domain.model.StatusProjeto;
