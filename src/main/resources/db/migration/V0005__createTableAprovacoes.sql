fe548b6fdf3dd3bfe805d476907150ac07815467
Jean <jeanrgl03@gmail.com>
2021-09-29 16:06:52 -0300

    Alteração no modelo de armazenamento de aprovaçẽos

--- src/main/resources/db/migration/V0005__createTableAprovacoes.sql
+++ src/main/resources/db/migration/V0005__createTableAprovacoes.sql
@@ -3,6 +3,7 @@ CREATE TABLE aprovacoes (
     data timestamp not null,
     nome_fornecedor varchar(120) not null,
     nome_responsavel varchar(120) not null,
+    id_consultor bigint not null,
     horas_aprovadas int not null,
     valor_horas double not null,
     primary key (id)
