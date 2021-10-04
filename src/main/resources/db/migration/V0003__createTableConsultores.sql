889259b4ce74c56be8242ef7f8b14cff40bcc4af
Jean <jeanrgl03@gmail.com>
2021-10-04 11:24:25 -0300

    Adição de valorHora na database

--- src/main/resources/db/migration/V0003__createTableConsultores.sql
+++ src/main/resources/db/migration/V0003__createTableConsultores.sql
@@ -2,6 +2,7 @@ CREATE TABLE consultores (
     cadastro_consultor bigint not null auto_increment,
     nome_consultor varchar(100) not null,
     limite_horas bigint not null,
+    valor_horas double not null,
     status varchar(7) not null,
     primary key (cadastro_consultor),
     user_id bigint
@@ -11,6 +12,7 @@ CREATE TABLE consultores (
         null,
         "Jean Henrique Reiguel",
         1600,
+        4.50,
         "ATIVO",
         1
     );
@@ -18,6 +20,7 @@ CREATE TABLE consultores (
         null,
         "Maria Gabriela de Souza Cruz",
         1200,
+        4.50,
         "ATIVO",
        2
     );
@@ -25,6 +28,7 @@ CREATE TABLE consultores (
         null,
         "Roberta Polez Bassegio",
         1800,
+        4.50,
         "ATIVO",
         3
     );
