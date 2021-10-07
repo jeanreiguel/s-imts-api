8f4ff2413e0659ac758b8ca3e76eb86983db1812
jenasw <jeanrgl03@gmail.com>
2021-10-07 10:20:04 -0300

    Alteração do modelo de armazenamento de informações dos responsáveis

--- src/main/resources/db/migration/V0010__createTableResponsaveis.sql
+++ src/main/resources/db/migration/V0010__createTableResponsaveis.sql
@@ -1,12 +1,12 @@
  CREATE TABLE fornecedor_responsaveis (
     id bigint not null auto_increment,
     id_fornecedor bigint not null,
-    nome varchar(50) not null,
+    nome_responsavel varchar(50) not null,
     user_id bigint,
     primary key(id)
 );
 alter TABLE fornecedor_responsaveis ADD CONSTRAINT fk_usuario_responsavel FOREIGN KEY (user_id) REFERENCES usuario(id);
 
-INSERT INTO fornecedor_responsaveis (id, id_fornecedor, nome, user_id) VALUES
+INSERT INTO fornecedor_responsaveis (id, id_fornecedor, nome_responsavel, user_id) VALUES
 (null,1, 'Jean Reiguel',1),
 (null,2,'Maria Gabriela',2);
