12ee41467d682c700ab3934963bd4f077b8b805c
jenasw <jeanrgl03@gmail.com>
2021-10-07 10:17:59 -0300

    Criação da exception destinada a Entidades não encontradas

---
+++ src/main/java/com/projetoWEG/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,6 @@
+package com.projetoWEG.domain.exception;
+
+public class EntidadeNaoEncontradaException extends  RuntimeException{
+
+    public EntidadeNaoEncontradaException(String message) { super(message); }
+}
